3bd5b93cbcfcfee658c34bea3229ba46efb71af2
Christian Peralta <christianipc.1987@gmail.com>
2024-10-30 13:03:45 -0300

    Hago una abstraccion del service de contacto con una interface

--- src/main/java/com/todotic/ContactListApi/service/ContactService.java
+++ src/main/java/com/todotic/ContactListApi/service/ContactService.java
@@ -2,95 +2,18 @@ package com.todotic.ContactListApi.service;
 
 import com.todotic.ContactListApi.dto.ContactDTO;
 import com.todotic.ContactListApi.entity.Contact;
-import com.todotic.ContactListApi.respository.ContactRepository;
-import lombok.AllArgsConstructor;
-import org.modelmapper.ModelMapper;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.multipart.MultipartFile;
-
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.time.LocalDateTime;
 import java.util.Optional;
 
-@AllArgsConstructor
-@Service
-public class ContactService {
 
-    //@Autowired
-    private final ContactRepository contactRepository;
-    private final ModelMapper mapper;
-    private final String uploadDirectory = "uploads";
+public interface ContactService {
 
-    public Iterable<Contact> findAll(){
-        return contactRepository.findAll();
-    }
-
-    public Contact findById(Long id){
-        //return contactRepository.findById(id).orElse(null);
-        return contactRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Contact not found with id: " + id));
-               // .orElseThrow(ResourceNotFoundException::new);// ::new representa al constructor
-    }
-
-    @Transactional
-    public Contact create(ContactDTO contactDTO) throws IOException {
-        /*String fileName = System.currentTimeMillis() + "_" + contactDTO.getFile().getOriginalFilename();
-        Path filePath = Path.of(uploadDirectory, fileName);
-
-        Files.createDirectories(filePath.getParent());
-        Files.write(filePath, contactDTO.getFile().getBytes());
-
-        Contact contact = mapper.map(contactDTO, Contact.class);
-        contact.setCreatedAt(LocalDateTime.now());*/
-
-        //contact.setPathUrl("uploads/" + fileName);
-        Contact contact = this.cargoImagen(contactDTO);
-        return contactRepository.save(contact);
-    }
-
-    public Contact update(Long id, ContactDTO contactDTO) throws IOException {
-        Contact contactoFromDb = this.findById(id);
-
-       // ModelMapper mapper = new ModelMapper();
-        mapper.map(contactDTO, contactoFromDb);
-
-        contactoFromDb.setName(contactDTO.getName());
-        contactoFromDb.setEmail(contactDTO.getEmail());
-
-        Contact contact = this.cargoImagen(contactDTO);
-        contactoFromDb.setPathUrl(contact.getPathUrl());
-        return contactRepository.save(contactoFromDb);
-    }
-
-    public void delete(Long id){
-        Contact contactoFromDb = this.findById(id);
-        contactRepository.delete(contactoFromDb);
-    }
-
-    public Optional<Contact> findContactByNameWithJPQL(String name){
-        return contactRepository.findContactByNameWithJPQL(name);
-    }
-
-    public Optional<Contact> findByName(String name){
-        return contactRepository.findByName(name);
-    }
-
-    Contact cargoImagen(ContactDTO contactDTO) throws IOException{
-        String fileName = System.currentTimeMillis() + "_" + contactDTO.getFile().getOriginalFilename();
-        Path filePath = Path.of(uploadDirectory, fileName);
-
-        Files.createDirectories(filePath.getParent());
-        Files.write(filePath, contactDTO.getFile().getBytes());
-
-        Contact contact = mapper.map(contactDTO, Contact.class);
-        contact.setCreatedAt(LocalDateTime.now());
-        contact.setPathUrl("uploads/" + fileName);
-        return contact;
-    }
+    Iterable<Contact> findAll();
+    Contact findById(Long id);
+    Contact create(ContactDTO contactDTO);
+    Contact update(Long id, ContactDTO contactDTO);
+    void delete(Long id);
+    Optional<Contact> findContactByNameWithJPQL(String name);
+    Optional<Contact> findByName(String name);
 }
 
 
