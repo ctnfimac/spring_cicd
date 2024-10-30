48a15cf26b1298e8efae954c86241874a3d7556f
Christian Peralta <christianipc.1987@gmail.com>
2024-10-30 19:07:09 -0300

    CI agrego modificado el test findByNameNotFound

--- src/test/java/com/todotic/ContactListApi/service/ContactServiceTest.java
+++ src/test/java/com/todotic/ContactListApi/service/ContactServiceTest.java
@@ -27,34 +27,36 @@ class ContactServiceTest {
     @Mock
     private ContactRepository contactRepository;
 
+    private Contact contact;
+
     @BeforeEach
     void setUp() {
-        Contact contact = Contact.builder()
+        contact = Contact.builder()
                 .name("Kamisama")
                 .email("kami@gmail.com")
                 .createdAt(LocalDateTime.now())
                 .build();
-        Mockito.when(contactRepository.findByName("Kamisama")).thenReturn(Optional.of(contact));
-
-        // retorno vacio si ingreso un nombre que no existe
-        //Mockito.when(contactRepository.findByName("Incorrecto")).thenReturn(Optional.empty());
     }
 
     @Test
     @DisplayName("Prueba de obtencion de un contacto enviando un nombre válido")
     public void findByNameFound(){
+        Mockito.when(contactRepository.findByName("Kamisama")).thenReturn(Optional.of(this.contact));
         String contactName = "Kamisama";
         Contact contact = contactService.findByName(contactName).orElseThrow(
                 () -> new AssertionError("El contacto no fue encontrado")
         );
         assertEquals(contactName, contact.getName());
     }
-    /*
+
     @Test
     @DisplayName("Prueba de obtencion de un contacto enviando un nombre no válido")
     public void findByNameNotFound(){
+        // retorno vacio si ingreso un nombre que no existe
+        Mockito.when(contactRepository.findByName("Incorrecto")).thenReturn(Optional.empty());
         String contactName = "Incorrecto";
         Optional<Contact> contact = contactService.findByName(contactName);
         assertEquals(contact, Optional.empty());
-    }*/
+
+    }
 }
